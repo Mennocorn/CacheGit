9afe07b644c073e18cbea956bad8f2e919e67578
Mennocorn <kilian.wiendl@gmx.de>
2022-03-30 17:02:00 +0200

    Initial Commit

---
+++ Cache.py
@@ -0,0 +1,28 @@
+HEAD
+from MediaManger import FileMaker
+import json
+
+
+class Cache:
+
+    def __init__(self, file: str):
+        self.file = FileMaker(file)
+        self.file.is_json()
+        self._cache = None
+
+    def load(self) -> None:
+        with open(str(self.file)) as file_to_load_from:
+            self._cache = json.load(file_to_load_from)
+
+    async def save(self):
+        with open(str(self.file), 'w') as file_to_save_to:
+            json.dump(self._cache, file_to_save_to, indent=4)
+
+    @property
+    def cache(self):
+        return self._cache
+
+    @cache.setter
+    def cache(self, value):
+        self._cache = value
+
